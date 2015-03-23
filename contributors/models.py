10223f787ea6a6daf9071af1c46301435b7ec8b8
Jensen Dempsey <jdemp@jensendempsey.com>
2015-03-23 17:44:15 -0500

    Added image to contributor model

--- contributors/models.py
+++ contributors/models.py
@@ -14,12 +14,11 @@ class Contributor(models.Model):
     middle_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50)
     suffix = models.CharField(max_length=5)
+    image = models.URLField() 
     twitter = models.SlugField(db_index=True)
     email = models.EmailField(max_length=75)
     bio = models.TextField()
     description = models.CharField(max_length=200)
-    # roll/editor
-    #picture (1)
     class_standing = models.CharField(max_length=2, choices=CLASS_STANDING_OPTIONS, blank=True)
 
     def __str__(self):
