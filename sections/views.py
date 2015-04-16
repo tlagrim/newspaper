0b7d373c02f491e902680eb8ad7429d9d4d4d22e
Daniel Porter <danielbowenporter@gmail.com>
2015-04-16 12:23:46 -0500

    Section pages now display the featured article and all other articles.
    Viewpoint -> Viewpoints

--- sections/views.py
+++ sections/views.py
@@ -3,12 +3,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from sections.models import Section, Category
+from articles.models import Article
+from featured.models import FeaturedArticle
 
 
 # Create your views here.
 def section_page(request, section_url):
     section = Section.objects.get(url=section_url)
-    return render(request, 'section.html', {'section': section})
+    featured_article = FeaturedArticle.objects.get(section=section).article
+    articles = Article.objects.filter(section=section).exclude(url=featured_article.url)
+
+    template_context = {}
+    template_context['section'] = section
+    template_context['featured_article'] = featured_article
+    template_context['articles'] = articles
+
+    return render(request, 'section.html', template_context)
 
 
 def category_page(request, category_url):
